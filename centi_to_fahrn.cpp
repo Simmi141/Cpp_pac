1ec08d6b29781540288d745a06bbae3c55f8fc15
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-19 11:44:43 +0530

    Add files via upload

---
+++ centi_to_fahrn.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+float fahrn,centigrade;
+
+cout<<"value in centigrade:";
+cin>>centigrade;
+fahrn=(centigrade*9/5)+32;
+cout<<fahrn<<endl;
+return 0;
+}
