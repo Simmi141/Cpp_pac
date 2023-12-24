ae7c128ef95eac3cb49e330fcff7f35e577f8add
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-24 22:41:26 +0530

    Add files via upload

---
+++ gross_salary.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+float distance,meter,feet,inch,centimeter;
+cout<<"enter the distance between two cities:";
+cin>>distance;
+meter=distance*1000;
+feet=
+
+}
