f1dfba72deb5a4345b24afb11981bccdd95b6ffa
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-25 23:38:38 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int num, odd,even;
+	cout<<"enter the number:"<<endl;
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<"number is even"<<endl;
+	
+	}
+	else 
+	{
+		cout<<"number is odd"<<endl;
+	}
+	return 0;
+}
