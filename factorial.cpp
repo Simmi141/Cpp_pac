9dcd9f20d3c687afcf2631ce0335f7312710632b
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-27 22:28:07 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,n,fact=1;
+	cout<<"enter the number:"<<endl;
+	cin>>n;
+	for(i=1;i<=n;i++){
+	
+	fact*=i;}
+	cout<<"factorial is: "<<fact;
+	
+
+return 0;
+}
