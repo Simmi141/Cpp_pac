6043406b94721e916a43f79790c5fc3f2f06df68
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-27 22:29:49 +0530

    Add files via upload

---
+++ interchange.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int c,d,temp;
+	cout<<"enter the value of c:"<<endl;
+	cin>>c;
+	cout<<"enter the value of d:"<<endl;
+	cin>>d;
+	c=c+d;
+	d=c-d;
+	c=c-d;
+	cout<<"d="<<d<<endl;
+	cout<<"c="<<c<<endl;
+	return 0;
+	
+}
