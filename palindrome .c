a03864660703706c1de14cc75bb1073c1f9e4ab9
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-28 22:28:35 +0530

    Add files via upload

---
+++ palindrome .c
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e,rev;
+	cout<<"enter five digit nymber: ";
+	cin>>a>>b>>c>>d>>e;
+	cout<<"rev is:"<<e<<d<<c<<b<<a<<endl;
+	return 0;
+	
+	
+}
