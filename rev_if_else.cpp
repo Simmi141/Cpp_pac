3f420e8fb14b1b7e5ebc98aad782f42db11ed770
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-31 23:20:30 +0530

    Add files via upload

---
+++ rev_if_else.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+		int a,b,c,d,e,rev;
+	cout<<"enter five digit nymber: ";
+	cin>>a>>b>>c>>d>>e;
+	cout<<"rev is:"<<e<<d<<c<<b<<a<<endl;
+	cin>>rev;
+	
+}
