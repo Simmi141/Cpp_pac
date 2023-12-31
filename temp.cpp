f52c586af775d9c9ea2c830188e026bef2e90475
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2023-12-31 23:36:20 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+float fahrn,centigrade;
+
+cout<<"value in fahrenheit:";
+cin>>fahrn;
+centigrade=(fahrn-32)*5/9;
+cout<<centigrade<<endl;
+return 0;
+}
