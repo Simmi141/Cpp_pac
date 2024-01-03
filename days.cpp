d1ea50c55a85f2b889a9dfc6d2f4433a4c532d94
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2024-01-03 19:19:36 +0530

    Add files via upload

---
+++ days.cpp
@@ -0,0 +1,29 @@
+
+
+#include<iostream>
+using namespace std;
+int main(){
+int day;
+cin>>day;
+switch(day){
+	case 1:
+	cout<<"monday"<<endl;
+	break;
+	
+	case 2:
+	cout<<"tuesday"<<endl;
+	break;
+	
+	case 3:
+	cout<<"wed"<<endl;
+	break;
+	
+	case 4:
+	cout<<"thurs"<<endl;
+	break;
+	
+	default:
+	cout<<"default case"<<endl;
+}
+return 0;
+}
